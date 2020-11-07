30eb787e1c0d0ff59d37cf7448c9adaff857aa07
CHEVALLIERCYBER <74060200+CHEVALLIERCYBER@users.noreply.github.com>
2020-11-07 11:55:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,34 +3,32 @@
 """
 Created on Thu Aug 27 11:37:12 2020
 
-@author: guillaume
+@author: guillaum
 """
 
 import entrainement as ent
 import modele as md
 import prediction as pr
 import traitement as tr
+import mim 
 from sklearn import tree
+
+################### TEST STATIQUE ####################
+# on teste le modele sur une capture existante
 
 #test=open("/home/guillaume/PFE/DATA/RMC/test_RMC.json") # capture de trames RMC uniquement
 #test_jamming=open("/home/guillaume/PFE/GNSS_LOG/GPS/01_09_2020_jamming_gps_rmc.log.json")
 test=open("/home/guillaume/PFE/DATA/RMC/test_RMC_jamming.json")
+
 test = tr.load(test)
 
-dict2=md.modele()
-modele=ent.entrainement(dict2) # calcule les valeurs de µ et sigma pour les différentes valeurs de vitesse et cap
+dict=md.modele()
+modele=ent.entrainement(dict) # calcule les valeurs de µ et sigma pour les différentes valeurs de vitesse et cap
 
-resultat_prediction=pr.prediction(test,modele)[0] #retourne une lisye de w,y,z
+resultat_prediction=pr.prediction(test,modele)[0] #retourne une lisye de w
 
+############### TEST DYNAMIQUE #####################
+# on test le modele en temps reel
 
-
-X=resultat_prediction[1]
-
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-
-#l= float(len(detection_jamming))
-#jamming= float(detection_jamming.count(True))
-
-#print("brouillage détécté : ", 100*jamming/l, "%")
+mim.mim()
 
