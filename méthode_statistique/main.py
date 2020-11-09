c14a4372e805c7402340fb4bfebea657a27849bb
CHEVALLIERCYBER <74060200+CHEVALLIERCYBER@users.noreply.github.com>
2020-11-09 10:27:10 +0100

    Update main.py

--- méthode_statistique/main.py
+++ méthode_statistique/main.py
@@ -1,40 +1,63 @@
+------------------------------------------------------------------------
+
+#
+
+# @Auteurs : EV2 CHEVALLIER 
+
+#
+
+# @Date : 06.11.20
+
+# @Lieu : École Navale / Chaire de Cyberdéfense des systèmes navals
+
+# @Cadre : Projet de Fin d'Études
+
+# @Sujet : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
+
+#
+
+#------------------------------------------------------------------------
+
+# @Titre : Main
+
+#------------------------------------------------------------------------
+
+# @Description : # Ce programme execute les différentes scripts necessaires au fonctionnement de l'algorithme et réalise un calcule de score.
+# Ces test ont lieu sur un fichier statique et en temps réél
+
+#------------------------------------------------------------------------
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 27 11:37:12 2020
-
-@author: guillaum
-"""
 
 import pickle as pk
 
 import entrainement as ent
 import modele as md
 import prediction as pr
+import prediction2 as pr2
 import traitement as tr
 import man_in_the_middle as mim
 
 ################### TEST STATIQUE ####################
-# on teste le modele sur une capture existante
+# on teste les modeles sur une capture existante
 
-#test=open("/home/guillaume/PFE/DATA/RMC/test_RMC.json") # capture de trames RMC uniquement
 
 test=open("/home/guillaume/PFE/DATA/RMC/test_RMC_jamming.json")
 
 test = tr.load(test)
-
-#dict=md.modele()
-#modele=ent.entrainement(dict) # calcule les valeurs de µ et sigma pour les différentes valeurs de vitesse et cap et cree un fichier modele.sauv
 
 modele_sauv=open("/home/guillaume/PFE/pythonProject/model.sauv","rb")
 
 modele=pk.load(modele_sauv)
 
 resultat_prediction=pr.prediction(test,modele) 
+resultat_prediction2=pr.prediction2(test,modele) 
+
 
 print(resultat_prediction) # retourne une liste de booleens en fonction d'un leurrage detecté ou non
 
 ############### TEST DYNAMIQUE #####################
 # on test le modele en temps reel
 
-#mim.mim()
+mim.mim()
