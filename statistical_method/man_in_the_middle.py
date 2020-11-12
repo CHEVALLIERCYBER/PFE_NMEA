1f17d717258d293e861c4fe053eba7279f956662
CHEVALLIERCYBER <74060200+CHEVALLIERCYBER@users.noreply.github.com>
2020-11-12 16:32:34 +0100

    Update man_in_the_middle.py

--- statistical_method/man_in_the_middle.py
+++ statistical_method/man_in_the_middle.py
@@ -1,17 +1,16 @@
 #------------------------------------------------------------------------
 #
-# @Auteurs : EV2 CHAVELLIER & EV2 LEBIGRE
+# @Auteurs : EV2 CHEVALLIER
 #
 # @Date : 06.11.20
 # @Lieu : École Navale / Chaire de Cyberdéfense des systèmes navals
 # @Cadre : Projet de Fin d'Études
-# @Sujet : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
-#
+# @Sujet : # Real time detection of cyber anomalies upon a NMEA network by using machine learning methods
 #------------------------------------------------------------------------
-# @Titre : Man in the Middle entre BridgeCommand et OpenCPN
+# @Titre : Man in the Middle beetwen BridgeCommand and OpenCPN
 #------------------------------------------------------------------------
-# @Description : Ce script écoute sur le port de sortie de BridgeCommand via un socket, intercepte les trames et en modifie certaines à la volée.
-# En parallèle, l'appel au script du module prediction permet de detecter via la méthode statistique si un leurrage a lieu  
+# @Description : This script listen the output port of Bridge Command with a socket, intercept and alter some randomly chosen NMEA sentences.
+# By the same time, the scripts prediction_v1 and prediction_v2 try to detect the modifications.
 #------------------------------------------------------------------------
 
 #!/usr/bin/env python3
@@ -20,22 +19,20 @@
 import socket
 import pynmea2
 import json
-import random as rd # modules internes
+import random as rd # internal modules
 import pickle as pk
 
-import traitement as tr # modules externes
+import traitement as tr # external modules
 import prediction as pr
 
-N=100 # pour l'évaluation des résultats du modele
-
-#lecture tps reel des trames
+N=102 # evaluation of 100 sentences, the two first sentences are ignored
 
 def mim():
 
 
     print("loading the model...")
 
-    infile = open("model.sauv", 'rb') # recupere le modele sauvegard dans le fichier objet model.sauv
+    infile = open("model.sauv", 'rb') # use the model saved in the file model.sauv
     modele = pk.load(infile)
     infile.close()
 
@@ -46,16 +43,16 @@ def mim():
     UDP_PORTrec = 5005
     UDP_PORTenv = 10110
 
-    sockrec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket pour recevoir les données issues de BridgeCommand
-    sockenv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket pour renvoyer les trames vers OpenCPN
+    sockrec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket to receive the sentences from Bridge Command 
+    sockenv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket to send the sentences to Open CPN
     sockrec.bind((UDP_IPrec, UDP_PORTrec))
 
     l_phi,l_g,l_t,l_v,l_trame=[],[],[],[],[]
     cap_test=[0,0]
-    l_faux_positif=[] # prediction=true et leurrage=false
-    l_faux_negatif=[] # prediction=false et leurrage=false
-    l_vrai_positif=[] # prediction=true et leurrage = true
-    l_vrai_negatif=[] # prediction=false et leurrage=true
+    l_faux_positif=[] # prediction=true and leurrage=false
+    l_faux_negatif=[] # prediction=false and leurrage=false
+    l_vrai_positif=[] # prediction=true and leurrage = true
+    l_vrai_negatif=[] # prediction=false and leurrage=true
     i=0
     nb_leurrage=0
 
@@ -68,19 +65,28 @@ def mim():
 
         if (message.sentence_type == "RMC"):
 
-            i += 1  # compte le nombre de trames
+            i += 1  # number of sentences
 
-            alea = 0#rd.randint(0, 3)
-            if (alea == 1):  # modifie aléatoirement des trames, 33% de trames fausses
-                offset_lat=0.00050 + 0.0045 # 10'  de lat
-                offset_lon=0.00050 + 0.0045 # 10'  de lon
-                modif_lat = float(message.data[2]) + offset_lat #
-                modif_lon = float(message.data[4]) + offset_lon
-                message.data[2] = str(modif_lat)
-                message.data[4] = str(modif_lon)
-                print("jamming : offset_lon=",offset_lon,"offset_lat=",offset_lat)
-                flag_leurrage = True
-                nb_leurrage += 1
+            
+            alea = rd.randint(0, 10)
+            
+            if (alea == 1 ):  # alter randomly some sentences 
+                offset_lat=0.05 # 10'  de lat
+                offset_lon = 0.0
+            elif (alea == 2):
+                offset_lat = 0.0
+                offset_lon=0.05 # 10'  de lon
+            elif (alea == 3) :
+                offset_lat = 0.05
+                offset_lon = 0.05
+                
+            modif_lat = float(message.data[2]) + offset_lat 
+            modif_lon = float(message.data[4]) + offset_lon
+            message.data[2] = str(modif_lat)
+            message.data[4] = str(modif_lon)
+            print("jamming : offset_lon=",offset_lon,"offset_lat=",offset_lat)
+            flag_leurrage = True
+            nb_leurrage += 1
 
             trame=json.loads(tr.dissect(message))
             trame2={}
@@ -91,24 +97,24 @@ def mim():
 
             if (trame["lat_dir"] == 'N'):
                 phi_str=(trame["lat"])
-                phi=float(phi_str[0:2])*60 + float(phi_str[2:]) # réalise des conversions en minutes d'angle
+                phi=float(phi_str[0:2])*60 + float(phi_str[2:]) # conversion in minutes
                 l_phi.append(phi)
                 trame2["phi"]=phi
-            else:  # si Phi est négatif 
+            else:  # if phi is negative
                 phi_str = (trame["lat"])
-                phi = -float(phi_str[0:2]) * 60 + float(phi_str[2:])  # en minutes de phi
+                phi = -float(phi_str[0:2]) * 60 + float(phi_str[2:])  # 
                 l_phi.append(phi)
                 trame2["phi"] = phi
 
             if (trame["lon_dir"] == 'W'):
                 g_str=(str(trame["lon"]))
-                g=float(g_str[0:3]) * 60 + float(g_str[3:]) # réalise des conversions en minute de g
+                g=float(g_str[0:3]) * 60 + float(g_str[3:]) 
                 l_g.append(g)
                 trame2["g"] = g
 
 
             else:
-                g=(-float(g_str[0:3]) * 60 + float(g_str[3:])) # si G est négatif
+                g=(-float(g_str[0:3]) * 60 + float(g_str[3:])) # if G is negative
                 l_g.append(g)
                 trame2["g"] = g
 
@@ -116,15 +122,15 @@ def mim():
             trame2["speed"] = float(trame["spd_over_grnd"])
             l_trame.append(trame2)
 
-            if i>=3: # il faut au moins 3 trames 
+            if i>=3: 
                 l_trame=l_trame[-3:]
 
                 l_phi=[l_trame[0]["phi"],l_trame[1]["phi"],l_trame[2]["phi"]]
-                l_g=[l_trame[0]["g"],l_trame[1]["g"],l_trame[2]["g"]]                              # conserve les deux dernières valeurs de phi et g
-                l_t=[l_trame[0]["timestamp"],l_trame[1]["timestamp"],l_trame[2]["timestamp"]]# conserve les 3 dernières valeurs connues de temps
-                l_v=[l_trame[0]["speed"],l_trame[1]["speed"],l_trame[2]["speed"]] # conserve les 3 dernières valeurs connues de vitesse
+                l_g=[l_trame[0]["g"],l_trame[1]["g"],l_trame[2]["g"]]                              # keep the two last values of phi and G
+                l_t=[l_trame[0]["timestamp"],l_trame[1]["timestamp"],l_trame[2]["timestamp"]]# keep the three last values of time
+                l_v=[l_trame[0]["speed"],l_trame[1]["speed"],l_trame[2]["speed"]] # keep the three last values of speed
  
-                cap=tr.cap(l_phi,l_g) # en minutes
+                cap=tr.cap(l_phi,l_g) 
                 cap_test.append(cap[-1])
                 cap_test.append(cap[-2])
                 cap_test=cap_test[-2:]
@@ -134,7 +140,7 @@ def mim():
                 leurrage = pr.prediction(test, modele)
                 print("leurrage : ", leurrage)
                 if flag_leurrage:
-                    l_trame.pop()  # on retire la trame leurrée
+                    l_trame.pop()  # we get out the spoofed sentence
 
                 if (leurrage and flag_leurrage):
                     l_vrai_positif.append(1)
@@ -162,6 +168,6 @@ def mim():
 
     print("faux_negatif:",faux_negatif)
     print("faux_positif:",faux_positif)
-    print("vrai_negatif:",vrai_negatif)  # on réalise les estimations sur 100 trames RMC
+    print("vrai_negatif:",vrai_negatif)  # compute the estimation upon 100 sentences
     print("vrai_positif:",vrai_positif)
     print(nb_leurrage)
