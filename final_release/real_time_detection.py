cb6774cc1e26ca416a29b1d72cbfa419654ed32d
Lebigre <74139424+Lebigre@users.noreply.github.com>
2020-11-12 16:03:50 +0100

    Update real_time_detection.py

--- final_release/real_time_detection.py
+++ final_release/real_time_detection.py
@@ -1,27 +1,27 @@
 
 #------------------------------------------------------------------------
 #
-# @Auteurs : EV2 CHEVALLIER & EV2 LEBIGRE
+# @Creators : EV2 CHEVALLIER & EV2 LEBIGRE
 #
 # @Date : 07.11.20
-# @Lieu : École Navale / Chaire de Cyberdéfense des systèmes navals
-# @Cadre : Projet de Fin d'Études
-# @Sujet : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
-#
+# @Location : École Navale / Chaire de Cyberdéfense des systèmes navals
+# @Project : End of studies project
+# @Subject : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
+#            # Detection of cyber anomalies in real time on NMEA network by using machine learning
 #------------------------------------------------------------------------
-# @Titre : Detection temps réél finale
+# @Title : real_time_detection
 #------------------------------------------------------------------------
-# @Description : Ce code est la version finale du programme de détection que nous avons créé. Il détecte en temps réél
-# les anomalies des trames NMEA fournies en entrée, sur le port 5005.
+# @Description : This code is the final version of the detection program that we created. It detects, in real time,
+# anomalies in NMEA data that are provided on port 5005 (you can can change the port at the start)
 #
-# Ce programme utilise les fichiers RMC_45min_dataset.json et RPM_45min_dataset.json comme données d'entrainement(modifiez le chemin d'accés des deux fichiers ligne 174 et 175).
-# En sortie, pour recuperer la classification faite par le programme, il suffit de recuperer les booléens brouildetec et
-# brouildeteccap, si l'un est vrai, un leurrage est détecté par le programme
+# This program uses files RMC_45min_dataset.json and RPM_45min_dataset.json as training data set
+# (specify the access path at the start).
+# To get the classification of the program, you just have to use variables spoof_detect_distance and
+# spoof_detect_heading, if one is TRUE, a spoofing is detected by the program.
 #
-# Une tentative de détection d'anomalies moteur sur les trames RPM est présente en commentaire
 #
+# A try of a detection on engine NMEA RPM data is in comment lines
 #------------------------------------------------------------------------
-
 
 import numpy as np
 import pynmea2
@@ -32,28 +32,41 @@ from sklearn import preprocessing
 from sklearn import svm
 
 
+##################################################################################################
 
-# calcul du cap (il faut trois latitude et trois longitude minimum pour calculer)
-def cap(l_phi, l_g):
+# loading of train set data, enter access path
+train_dataRMC = open('')
+train_dataRPM = open('')
+
+# port used
+UDP_IPrec = "127.0.0.1" # IP adress
+UDP_PORTrec = 5005  # entry socket (bridge command)
+
+#################################################################################################
+
+
+
+# Heading computation (three latitudes and three longitude are needed to process the heading)
+def heading(l_phi, l_g):
     epsilon = 10 ** (-13)
-    capcalcul = []
+    list_heading_calculated = []
     for i in range(len(l_phi) - 1):
 
         if abs(l_phi[i + 1] - l_phi[i]) < epsilon and l_g[i + 1] <= l_g[i]:
-            cv = 90
-            capcalcul.append(cv)
+            heading_calculated = 90
+            list_heading_calculated.append(heading_calculated)
 
         elif abs(l_phi[i + 1] - l_phi[i]) < epsilon and l_g[i + 1] > l_g[i]:
-            cv = 270
-            capcalcul.append(cv)
+            heading_calculated = 270
+            list_heading_calculated.append(heading_calculated)
 
         elif l_phi[i + 1] >= l_phi[i] and abs(l_g[i + 1] - l_g[i]) < epsilon:
-            cv = 0
-            capcalcul.append(cv)
+            heading_calculated = 0
+            list_heading_calculated.append(heading_calculated)
 
         elif l_phi[i + 1] < l_phi[i] and abs(l_g[i + 1] - l_g[i]) < epsilon:
-            cv = 180
-            capcalcul.append(cv)
+            heading_calculated = 180
+            list_heading_calculated.append(heading_calculated)
 
         else:
 
@@ -61,88 +74,88 @@ def cap(l_phi, l_g):
                 math.cos(0.5 * (l_phi[i + 1] + l_phi[i])) * (l_phi[i + 1] - l_phi[i]) / (l_g[i + 1] - l_g[i])))
 
             if l_phi[i + 1] > l_phi[i] and l_g[i + 1] < l_g[i]:
-                cv = 90 - a
-                capcalcul.append(cv)
+                heading_calculated = 90 - a
+                list_heading_calculated.append(heading_calculated)
 
             elif l_phi[i + 1] > l_phi[i] and l_g[i + 1] > l_g[i]:
-                cv = 270 + a
-                capcalcul.append(cv)
+                heading_calculated = 270 + a
+                list_heading_calculated.append(heading_calculated)
 
             elif l_phi[i + 1] < l_phi[i] and l_g[i + 1] > l_g[i]:
-                cv = 270 - a
-                capcalcul.append(cv)
+                heading_calculated = 270 - a
+                list_heading_calculated.append(heading_calculated)
 
             else:  # l_phi[i+1]>l_phi[i] and l_g[i+1]<l_g[i]:
-                cv = 90 + a
-                capcalcul.append(cv)
-    return capcalcul
+                heading_calculated = 90 + a
+                list_heading_calculated.append(heading_calculated)
+    return list_heading_calculated
 
 
-# calcul distance
+# distance computation
 def distance(phi1, phi0, g1, g0):
     return np.sqrt(
         (float(phi1) - float(phi0)) ** 2 + ((float(g1) - float(g0)) / ((float(phi1) + float(phi0)) / 2)) ** 2)
 
 
-def diffliste(liste):
-    listediff = []
-    for i in range(1, len(liste) - 1):
-        listediff.append(liste[i] - liste[i - 1])
-    return listediff
+def diff_list(list):
+    list_diff = []
+    for i in range(1, len(list) - 1):
+        listediff.append(list[i] - list[i - 1])
+    return list_diff
 
 
-# mise en forme des données RMC, preprocessing
-def loadnprocessRMC(file):
+# loading and preprocessing of RMC data for training
+def load_and_process_RMC(file):
     list_phi = []
     list_g = []
     list_t = []
     list_v = []
-    resultat = [[]]
+    result = [[]]
     for line in file:
-        trame = json.loads(line)
-        list_phi.append(float(trame["lat"]))
-        list_g.append(float(trame["lon"]))
-        list_t.append(float(trame["timestamp"]))
-        list_v.append(float(trame["spd_over_grnd"]))
-    resultat.append(list_phi)
-    resultat.append(list_g)
-    resultat.append(list_t)
-    resultat.append(list_v)
-    resultat.pop(0)
+        data = json.loads(line)
+        list_phi.append(float(data["lat"]))
+        list_g.append(float(data["lon"]))
+        list_t.append(float(data["timestamp"]))
+        list_v.append(float(data["spd_over_grnd"]))
+    result.append(list_phi)
+    result.append(list_g)
+    result.append(list_t)
+    result.append(list_v)
+    result.pop(0)
     dist = [0, 0]
-    for i in range(1, len(resultat[0]) - 1):
-        dist.append(distance(resultat[0][i], resultat[0][i - 1], resultat[1][i], resultat[1][i - 1]))
+    for i in range(1, len(result[0]) - 1):
+        dist.append(distance(result[0][i], result[0][i - 1], result[1][i], result[1][i - 1]))
     matrix1 = np.zeros((len(dist), 2))
-    listecap = cap(list_phi, list_g)
-    diffecap = diffliste(listecap)
+    listecap = heading(list_phi, list_g)
+    diffecap = diff_list(listecap)
     for i in range(0, len(dist) - 1):
-        matrix1[i][0] = (resultat[3][i])
+        matrix1[i][0] = (result[3][i])
         matrix1[i][1] = dist[i]
     i = 0
-    fin = len(matrix1) - 1
-    # preprocessing du data train : suppression des points abérants
-    while i < fin:
+    end = len(matrix1) - 1
+    # preprocessing of training data : outliers are deleted
+    while i < end:
         if matrix1[i][1] > 7:
             matrix1 = np.delete(matrix1, i, 0)
-            fin = fin - 1
+            end = end - 1
         i = i + 1
     matrix2 = np.zeros((len(diffecap), 2))
     for i in range(0, len(diffecap) - 1):
-        matrix2[i][0] = (resultat[3][i])
+        matrix2[i][0] = (result[3][i])
         matrix2[i][1] = diffecap[i]
 
     return matrix1, matrix2
 
 
-def loadnprocessRPM (file):
+def load_and_process_RPM (file):
     list_rpm_eng1 = []
     list_rpm_eng2 = []
     for line in file:
-        trame = json.loads(line)
-        if trame["engine_no"] == "1":
-            list_rpm_eng1.append(float(trame["speed"]))
+        data = json.loads(line)
+        if data["engine_no"] == "1":
+            list_rpm_eng1.append(float(data["speed"]))
         else:
-            list_rpm_eng2.append(float(trame["speed"]))
+            list_rpm_eng2.append(float(data["speed"]))
     diffeng1=[]
     diffeng2=[]
     for i in range(0, len(list_rpm_eng1)-2):
@@ -159,21 +172,18 @@ def loadnprocessRPM (file):
 
 
 
-# chargement des données du train set
-train_dataRMC = open('')
-train_dataRPM = open('')
 
 # normalisation
-dataRMC = loadnprocessRMC(train_dataRMC)
-dataRPM = loadnprocessRPM(train_dataRPM)
+dataRMC = load_and_process_RMC(train_dataRMC)
+dataRPM = load_and_process_RPM(train_dataRPM)
 
 X_train = dataRMC[0]
 X_traincap = dataRMC[1]
 X_trainRMC = dataRPM[0]
 
-scaler = preprocessing.StandardScaler().fit(X_train)  # definition du "normalisateur" pour la distance
-scaler1 = preprocessing.StandardScaler().fit(X_traincap)  # definition du "normalisateur" pour les caps
-scalerRPM = preprocessing.StandardScaler().fit(X_trainRMC)  # definition du "normalisateur" pour les caps
+scaler_distance = preprocessing.StandardScaler().fit(X_train)  # definition du "normalisateur" pour la distance
+scaler_heading = preprocessing.StandardScaler().fit(X_traincap)  # definition du "normalisateur" pour les caps
+scaler_RPM = preprocessing.StandardScaler().fit(X_trainRMC)  # definition du "normalisateur" pour les caps
 X_train = preprocessing.scale(X_train)
 X_traincap = preprocessing.scale(X_traincap)
 X_trainRMC = preprocessing.scale(X_trainRMC)
@@ -187,99 +197,90 @@ clfeng = svm.OneClassSVM(nu=0.007, kernel="rbf", gamma=0.5)
 clfeng.fit(X_trainRMC)
 
 
-# definition des ports
-UDP_IPrec = "127.0.0.1"
-UDP_PORTrec = 5005  # socket entrée (bridge command)
-
 print("UDP receive IP: %s" % UDP_IPrec)
 print("UDP receive port: %s" % UDP_PORTrec)
-
-
-# création des socket d'entrée et sortie
 sockrec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 sockrec.bind((UDP_IPrec, UDP_PORTrec))
 
-departdiff = 3  # compteur de trame avant debut du test
-departdiffRPM = 2
-ntrameRMC = 0  # compteur de trame
-ntrameRPM = 0
-donneestest = []  # liste contenant les données en cours, à tester
-donneestestRPM = []  # liste contenant les données RPM en cours, à tester
+differed_start_RMC = 3  # number of RMC data before test (must be at least 3)
+differed_strat_RPM = 2  # number of RMC data before test (must be at least 3)
+n_data_RMC = 0
+n_data_RPM = 0
+data_test_RMC = []  # list of current RMC data to test
+data_test_RPM = []  # list of current RPM data to test
 
 try:
     while True:
 
-        # reception des données, parsing
+        # reception of data, parsing
         data = sockrec.recvfrom(1024)
         data = data[0].decode("utf-8")
         message = pynmea2.parse(data)
 
-        #test pour donnée RPM
+        # test for RPM data
         # if message.sentence_type == "RMC":
-        #     ntrameRPM = ntrameRPM + 1
-        #     donneestestRPM.append(message.data[2])
-        # if message.sentence_type == "RMC" and ntrameRMC >= departdiffRPM - 1:
-        #     ntrameRPM = ntrameRPM + 1
-        #     donneestestRPM.append(message.data[2])
-        #     diffRPM = float(donneestestRPM[0])-float(donneestestRPM[1])
-        #     y_pred_test = clfeng.predict(scalerRPM.transform([[diffRPM]]))
-        #     if y_pred_test == -1:
-        #         brouildetecmoteur = True
+        #     n_data_RPM = n_data_RPM + 1
+        #     data_test_RPM.append(message.data[2])
+        # if message.sentence_type == "RMC" and n_data_RMC >= differed_strat_RPM - 1:
+        #     n_data_RPM = n_data_RPM + 1
+        #     data_test_RPM.append(message.data[2])
+        #     diffRPM = float(data_test_RPM[0])-float(data_test_RPM[1])
+        #     y_pred_test_distance = clfeng.predict(scaler_RPM.transform([[diffRPM]]))
+        #     if y_pred_test_distance == -1:
+        #         spoofingdetectengine = True
         #         print("Tout va bien pour le moteur")
         #     else:
-        #         brouildetecmoteur = False
+        #         spoofingdetectengine = False
         #         print("Leurrage détécté  moteur !")
 
-        # test pour donnée RMC
-        # sequence de comptage des première trames, initialisation des données
+        # test for RMC data
+        # initialization
         if message.sentence_type == "RMC":
-            ntrameRMC = ntrameRMC + 1
-        if message.sentence_type == "RMC" and ntrameRMC == departdiff - 2:
-            donneestest.append(message.data[2])
-            donneestest.append(message.data[4])
-        if message.sentence_type == "RMC" and ntrameRMC == departdiff - 1:
-            donneestest.append(message.data[2])
-            donneestest.append(message.data[4])
-        # modification de trame, brouillage avec un offset de latitude et longitude
-        if message.sentence_type == "RMC" and ntrameRMC >= departdiff:
-            donneestest.append(message.data[2])
-            donneestest.append(message.data[4])
-            distancepoint = distance(donneestest[2], donneestest[4], donneestest[3], donneestest[5])
-            capinit = cap([float(donneestest[0]), float(donneestest[2])],
-                          [float(donneestest[1]), float(donneestest[3])])
-            capfin = cap([float(donneestest[2]), float(donneestest[4])], [float(donneestest[3]), float(donneestest[5])])
-            diffcap = float(capfin[0]) - float(capinit[0])
-            X_test = [[message.data[6], distancepoint]]
-            X_testcap = [[message.data[6], diffcap]]
-            y_pred_test = clf.predict(scaler.transform(X_test))  # prediction (apres normalisation)
-            y_pred_testcap = clfcap.predict(scaler1.transform(X_testcap))  # prediction (apres normalisation)
+            n_data_RMC = n_data_RMC + 1
+        if message.sentence_type == "RMC" and n_data_RMC == differed_start_RMC - 2:
+            data_test_RMC.append(message.data[2])
+            data_test_RMC.append(message.data[4])
+        if message.sentence_type == "RMC" and n_data_RMC == differed_start_RMC - 1:
+            data_test_RMC.append(message.data[2])
+            data_test_RMC.append(message.data[4])
 
-            if y_pred_test == -1 and y_pred_testcap == -1:
-                brouildeteccap = True
-                brouildetec = True  # le point est une anomalie de distance et cap
-                print('Leurrage détecté distance et cap !')
-            elif y_pred_testcap == -1 and y_pred_test == 1:
-                brouildetec = False
-                brouildeteccap = True  # le point est une anomalie de cap
-                print('Leurrage détecté cap !')
-            elif y_pred_testcap == 1 and y_pred_test == -1:
-                brouildetec = True
-                brouildeteccap = False  # le point est une anomalie de distance
-                print('Leurrage détecté distance !')
+        # add current data
+        if message.sentence_type == "RMC" and n_data_RMC >= differed_start_RMC:
+            data_test_RMC.append(message.data[2])
+            data_test_RMC.append(message.data[4])
+            distancepoint = distance(data_test_RMC[2], data_test_RMC[4], data_test_RMC[3], data_test_RMC[5])
+            capinit = heading([float(data_test_RMC[0]), float(data_test_RMC[2])],
+                          [float(data_test_RMC[1]), float(data_test_RMC[3])])
+            capfin = heading([float(data_test_RMC[2]), float(data_test_RMC[4])], [float(data_test_RMC[3]), float(data_test_RMC[5])])
+            difference_heading = float(capfin[0]) - float(capinit[0])
+            X_test_distance = [[message.data[6], distancepoint]]
+            X_test_heading = [[message.data[6], difference_heading]]
+
+            # prediction on current data
+            y_pred_test_distance = clf.predict(scaler_distance.transform(X_test_distance))
+            y_pred_test_heading = clfcap.predict(scaler_heading.transform(X_test_heading))
+
+            if y_pred_test_distance == -1 and y_pred_test_heading == -1:
+                spoof_detect_heading = True
+                spoof_detect_distance = True  # le point est une anomalie de distance et heading
+                print('Spoofing detected, distance and heading !')
+            elif y_pred_test_heading == -1 and y_pred_test_distance == 1:
+                spoof_detect_distance = False
+                spoof_detect_heading = True  # le point est une anomalie de heading
+                print('Spoofing detected, heading !')
+            elif y_pred_test_heading == 1 and y_pred_test_distance == -1:
+                spoof_detect_distance = True
+                spoof_detect_heading = False  # le point est une anomalie de distance
+                print('Spoofing detected, distance !')
             else:
-                brouildetec = False  # le point est bon
-                brouildeteccap = False
-                print('Tout va bien pour la position')
+                spoof_detect_distance = False  # le point est bon
+                spoof_detect_heading = False
+                print('Everything is find for position...')
 
 
-            # mise en memoire des données des points pour calcul de la prochaine distance
-            donneestest.pop(0)
-            donneestest.pop(0)
-
-        #En sortie, il suffit de recuperer les booléens brouildetec, brouildeteccap si l'un est vrai,
-        #Un leurage est détecté par le programme
-
+            # delete old data
+            data_test_RMC.pop(0)
+            data_test_RMC.pop(0)
 
 except KeyboardInterrupt:
     raise
