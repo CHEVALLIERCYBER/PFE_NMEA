20c40ed0e875929b44b690c8d557078847360acc
CHEVALLIERCYBER <74060200+CHEVALLIERCYBER@users.noreply.github.com>
2020-11-12 16:13:40 +0100

    Update main.py

--- statistical_method/main.py
+++ statistical_method/main.py
@@ -1,17 +1,18 @@
 #------------------------------------------------------------------------
 #
-# @Auteurs : EV2 CHEVALLIER 
+# @Author : EV2 CHEVALLIER 
 #
 # @Date : 06.11.20
-# @Lieu : École Navale / Chaire de Cyberdéfense des systèmes navals
+# @Location : École Navale / Chaire de Cyberdéfense des systèmes navals
 # @Cadre : Projet de Fin d'Études
-# @Sujet : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
+# @Subject : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
 #
 #------------------------------------------------------------------------
-# @Titre : Main
+# @Title : Main
 #------------------------------------------------------------------------
-# @Description : # Ce programme execute les différentes scripts necessaires au fonctionnement de l'algorithme et réalise un calcule de score.
-# Ces test ont lieu sur un fichier statique et en temps réél
+# @Description : # This programm execute the scripts to implement the statistical method, and compute a score calculation
+
+# These test are real time
 #------------------------------------------------------------------------
 
 #!/usr/bin/env python3
@@ -19,33 +20,17 @@
 
 import pickle as pk
 
-import entrainement as ent
-import modele as md
-import prediction as pr
-import prediction2 as pr2
+import training as ent
+import model as md
+import prediction_v1 as pr1
+import prediction_v2 as pr2
 import traitement as tr
 import man_in_the_middle as mim
 
-chemin="/home/guillaume/PFE/pythonProject" # mettre le chemin du dossier PFE_NMEA-main
-
-################### TEST STATIQUE ####################
-# on teste les modeles sur une capture existante
+path="/home/guillaume/PFE/pythonProject" # mettre le chemin du dossier PFE_NMEA-main
 
 
-test=open(chemin+"/PFE_NMEA-main/dataset/test_RMC_spoofing.json")
-
-test = tr.load(test)
-
-modele_sauv=open(chemin+"/PFE_NMEA-main/dataset/model.sauv","rb")
-
-modele=pk.load(modele_sauv)
-
-resultat_prediction=pr.prediction(test,modele)    # modele avec ecart en phi et g
-resultat_prediction2=pr2.prediction(test,modele)  # modele avec ecart en cap et distance
-
-print(resultat_prediction) # retourne une liste de booleens en fonction d'un leurrage detecté ou non
-
-############### TEST DYNAMIQUE #####################
-# on test le modele en temps reel
+############### DYNAMICAL TEST #####################
+# real time test
 
 #mim.mim()
