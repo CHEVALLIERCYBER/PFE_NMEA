33a3cf013f2a6b5db7611238053a6cb4904d9cab
CHEVALLIERCYBER <74060200+CHEVALLIERCYBER@users.noreply.github.com>
2020-11-12 16:42:09 +0100

    Update traitement.py

--- statistical_method/traitement.py
+++ statistical_method/traitement.py
@@ -1,16 +1,15 @@
 #------------------------------------------------------------------------
 #
-# @Auteurs : EV2 CHAVELLIER
+# @Author : EV2 CHEVALLIER
 #
 # @Date : 06.11.20
-# @Lieu : École Navale / Chaire de Cyberdéfense des systèmes navals
+# @Location : École Navale / Chaire de Cyberdéfense des systèmes navals
 # @Cadre : Projet de Fin d'Études
-# @Sujet : # Détection temps-réel d’anomalies cyber # sur un réseau NMEA par l’utilisation de # techniques d’apprentissage automatique.
-#
+# @Subject : # Real time detection of cyber anomalies upon a NMEA network by using machine learning methods
 #------------------------------------------------------------------------
-# @Titre : Prediction de leurrage
+# @Title : useful functions
 #------------------------------------------------------------------------
-# @Description : # Ce programme définit les fonctions statistiques utiles pour les calculs suivants : moyenne, écart-type, différence entre deux valeurs successives dans une liste 
+# @Description : # This programm implement some useful functions : statistic calculations, opening of a json file, difference beetwen two lists... 
 #------------------------------------------------------------------------
 
 import json
@@ -19,16 +18,20 @@ import statistics as st
 
 epsilon=0.0000000000001
 
-def dissect(messages):
+def set_path():
+    chemin="/home/guillaume/PFE/pythonProject/PFE_NMEA-main/" # put the correct path
+    return chemin
+
+def dissect(messages): # open a NMEA message formated with the python librairy pynmea2
     dico = []
     for i in range(len(messages.data) - 1):
         dico.append((messages.fields[i][1], messages.data[i]))
 
     dico = dict(dico)
-    return json.dumps(dico) # retourne
+    return json.dumps(dico) # 
 
 
-def load(file):
+def load(file):  # open a json file containing NMEA sentences
     list_phi = []
     list_g = []
     list_t = []
@@ -38,7 +41,6 @@ def load(file):
     resultat=[[]]
     for line in file:
         trame = json.loads(line)
-        #print(str(trame['status']))
 
         if str(trame['status'])=="A":
 
@@ -63,46 +65,18 @@ def load(file):
     resultat.append(list_cap)
 
     resultat.pop(0)
-    return resultat # retourne les listes de chaque parametres : phi g et t
+    return resultat 
 
-def load_gpxxx(file):
-    list_phi = []
-    list_g = []
-    list_t = []
-
-    resultat=[[]]
-    for line in file:
-        trame = json.loads(line)
-        if (trame["lat_dir"]=='N'):
-            list_phi.append(float(trame["lat"]))
-        else:
-            list_phi.append(-float(trame["lat"]))
-
-        if (trame["lon_dir"] == 'W'):
-            list_g.append(float(trame["lon"]))
-        else:
-            list_g.append(-float(trame["lon"]))
-
-        list_g.append(float(trame["lon"]))
-        list_t.append(float(trame["timestamp"]))
-
-    resultat.append(list_phi)
-    resultat.append(list_g)
-    resultat.append(list_t)
-
-    resultat.pop(0)
-    return resultat # retourne les listes de chaque parametres : phi g et t
-
-def delta(liste,list_t):  # list_phi ou list_g en minutes
+def delta(liste,list_t):  # take a list of latitude or longitude and a list of time
     delta_liste = []
     for i in (range(len(liste) - 1)):
         if (list_t[i + 1] - list_t[i]) !=0:
-            delta_liste.append( (liste[i + 1] - liste[i]) / (list_t[i + 1] - list_t[i]))  # écart en minutes d'arc
+            delta_liste.append( (liste[i + 1] - liste[i]) / (list_t[i + 1] - list_t[i]))  
         else:
             delta_liste.append(0)
     return delta_liste
 
-def delta_distance(l_phi,l_g): # retourne la liste des distances entre les pts successsifs
+def delta_distance(l_phi,l_g): # return the list of the distances beetwen successives points
     delta_d = []
     for i in range(len(l_phi)-1):
         phi_m=(l_phi[i+1]+l_phi[i])*0.5
@@ -110,7 +84,7 @@ def delta_distance(l_phi,l_g): # retourne la liste des distances entre les pts s
         delta_d.append(d)
     return delta_d
 
-def cap(l_phi,l_g): # phi g en minutes
+def cap(l_phi,l_g):  # compute the list of the headings
 
     cap=[]
 
@@ -162,9 +136,9 @@ def cap(l_phi,l_g): # phi g en minutes
 
     return cap
 
-def vitesse(l_phi,l_g,l_t):
+def speed(l_phi,l_g,l_t):
 
-    vitesse=[]
+    speed=[]
 
     for i in range(len(l_phi)-1):
         if abs(l_t[i+1]-l_t[i])>epsilon:
@@ -175,11 +149,11 @@ def vitesse(l_phi,l_g,l_t):
             delta_g=l_g[i+1]-l_g[i]
             print("delta_t : ", delta_t)
 
-            vf=60*60.*(math.sqrt(delta_phi**2 + (delta_g/cos_phi_m)**2) / delta_t )# duree en s
-            vitesse.append(vf)
+            vf=60*60.*(math.sqrt(delta_phi**2 + (delta_g/cos_phi_m)**2) / delta_t )# duration in s
+            speed.append(vf)
         else:
-            vitesse.append(0)
-    return vitesse
+            speed.append(0)
+    return speed
 
 def parametres(array_data):
 
@@ -188,5 +162,3 @@ def parametres(array_data):
     res["ecart-type"]=math.sqrt(st.variance(array_data))
     return res
 
-test_jamming=open("/home/guillaume/PFE/GNSS_LOG/GPS/01_09_2020_jamming_gps_rmc.log.json")
-load(test_jamming)
