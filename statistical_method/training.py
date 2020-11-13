2ba21b196ddfc971685f3c32457210bad394be65
CHEVALLIERCYBER <74060200+CHEVALLIERCYBER@users.noreply.github.com>
2020-11-13 09:22:47 +0100

    Update training.py

--- statistical_method/training.py
+++ statistical_method/training.py
@@ -1,18 +1,19 @@
 #------------------------------------------------------------------------
 #
-# @Auteurs : EV2 CHAVELLIER 
+# @Author : EV2 CHEVALLIER 
 #
-# @Date : 06.11.20
+# @Date : 16.09.20
 # @Location : École Navale / Chaire de Cyberdéfense des systèmes navals
-# @Cadre : Projet de Fin d'Études
+# @Project : Projet de Fin d'Études
 # @Subject : # Real time detection of cyber anomalies upon a NMEA network by using machine learning methods
 #
 #------------------------------------------------------------------------
 # @Title : Training
 #------------------------------------------------------------------------
-# @Description : # Ce programme récupère les données d'éntrainement sous forme d'un dictionnaire et calcule les parametres satistiques des features interessants : 
-# variations de phi, variation de g, variation de cap et variation de distance.
-# Le tout est sauvegardé dans un fichier binaire grace au module pickle de python.
+# @Description : # This programm get the training dataset, extract the interesting features ( mean and normal deviation of variations of latitude, 
+# longitude, heading and distance )
+# and put it in a python dictionnary and save it in a binary file with the pickle module.
+
 #------------------------------------------------------------------------
 
 
@@ -27,43 +28,43 @@ def training(dict2):
     model["µ"]={}
     model["sigma"]={}
 
-    for x in dict: # on parcourt le dictionnaire en fonction du champ de vitesse
+    for x in dict: # loop with speed
         model["µ"][x]={}
         model["sigma"][x]={}
 
-        for y in dict[x]: # on parcourt le dictionnaire en fonction du champ de cap
+        for y in dict[x]: # loop with heading
 
             model["µ"][x][y] = {}
             model["sigma"][x][y] = {}
 
-            doc=tr.load(dict[x][y]) # on ouvre le fichier json correspondant
+            doc=tr.load(dict[x][y]) # open the json file
 
             phi_l=doc[0]
-            g_l=doc[1]   # recupere une liste de phi,g,t
+            g_l=doc[1]   # get a list of phi,g,t
             t_l=doc[2]
 
-            dphi_l=tr.delta(phi_l,t_l) # calcule les différentiels
+            dphi_l=tr.delta(phi_l,t_l) # compute the differences
             dg_l=tr.delta(g_l,t_l)
             dcap_l=tr.delta(tr.cap(phi_l,g_l),t_l)
             d_distance=tr.delta_distance(phi_l,g_l)
 
-# on construit le modele avec les features que l'on veut : ici différence de cap et difference de distance mais aussi variation de phi et de g
+# we build a model with the statistical values of the features : variation of latitude, longitude, heading and distance
 
-            model["µ"][x][y]["phi"] = tr.parametres(dphi_l)["moyenne"]
-            model["µ"][x][y]["g"] = tr.parametres(dg_l)["moyenne"]    # met à jour le modele
+            model["µ"][x][y]["phi"] = tr.parametres(dphi_l)["mean"]
+            model["µ"][x][y]["g"] = tr.parametres(dg_l)["mean"]    # met à jour le modele
 
-            model["sigma"][x][y]["phi"] = tr.parametres(dphi_l)["ecart-type"]
-            model["sigma"][x][y]["g"] = tr.parametres(g_l)["ecart-type"]
+            model["sigma"][x][y]["phi"] = tr.parametres(dphi_l)["normal_deviation"]
+            model["sigma"][x][y]["g"] = tr.parametres(g_l)["normal_deviation"]
 
 
-            model["µ"][x][y]["cap"] = tr.parametres(dcap_l)["moyenne"]
-            model["µ"][x][y]["distance"] = tr.parametres(d_distance)["moyenne"]
+            model["µ"][x][y]["heading"] = tr.parametres(dcap_l)["mean"]
+            model["µ"][x][y]["distance"] = tr.parametres(d_distance)["mean"]
 
-            model["sigma"][x][y]["cap"] = tr.parametres(dcap_l)["ecart-type"]
-            model["sigma"][x][y]["distance"] = tr.parametres(d_distance)["ecart-type"]
+            model["sigma"][x][y]["heading"] = tr.parametres(dcap_l)["normal_deviation"]
+            model["sigma"][x][y]["distance"] = tr.parametres(d_distance)["normal_deviation"]
 
-    with open('model.sauv','wb' ) as model_sauv_file: # enregistre le modele dans un fichier
-        pk.dump(model, model_sauv_file) # sauvegarde le modele
+    with open('model.sauv','wb' ) as model_sauv_file: 
+        pk.dump(model, model_sauv_file) # save the model in a binary file
 
     return model
 
